eebad1044774558ef261a349f7703f5b3790c7bd
Miguel Silva <miguel1998282@gmail.com>
2022-03-13 22:38:33 +0000

    More efficient

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,39 +6,26 @@
 /*   By: mtavares <mtavares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 19:02:27 by mtavares          #+#    #+#             */
-/*   Updated: 2022/03/12 23:30:03 by mtavares         ###   ########.fr       */
+/*   Updated: 2022/03/13 22:37:22 by mtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//Another idea that i have for pontentially more performance
-/* void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	if (!dst && !src)
-		return (NULL);
-	while (i < n)
-	{
-		*(char *)(dst + i) = *(char *)(src + i);
-		i++;
-	}
-	return (dst);
-} */
-
-//this way passed moulinette
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
+	char	*d;
+	char	*s;
 
-	i = -1;
+	d = dst;
+	s = (char *)src;
 	if (!dst && !src)
 		return (NULL);
-	while (*(const char *)(src + i) && i < n)
+	i = 0;
+	while (s[i] && i < n)
 	{
-		*(char *)(dst + i) = *(const char *)(src + i);
+		d[i] = s[i];
 		i++;
 	}
 	return (dst);
