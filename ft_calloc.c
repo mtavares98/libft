53e2e829b60435939620d26a970fbe945b7de3ba
Miguel Silva <miguel1998282@gmail.com>
2022-04-14 15:08:30 +0100

    Calloc changed now work with overflow

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,25 @@
 /*   By: mtavares <mtavares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 14:16:53 by mtavares          #+#    #+#             */
-/*   Updated: 2022/03/07 18:35:27 by mtavares         ###   ########.fr       */
+/*   Updated: 2022/04/13 19:49:48 by mtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdint.h>
+#include <limits.h>
+#include <stdio.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	size_t	total;
 	void	*ptr;
 
-	ptr = (void *)malloc((count * size));
-	if (!ptr)
+	total = count * size;
+	if (total / size != count)
 		return (NULL);
-	ft_bzero(ptr, count * size);
+	ptr = malloc(total);
+	if (ptr)
+		ft_bzero(ptr, total);
 	return (ptr);
 }
--- ft_split.c
+++ ft_split.c
@@ -6,51 +6,68 @@
 /*   By: mtavares <mtavares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 14:45:03 by mtavares          #+#    #+#             */
-/*   Updated: 2022/04/12 18:40:13 by mtavares         ###   ########.fr       */
+/*   Updated: 2022/04/13 20:33:05 by mtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-void	ft_free(char **new)
+static void	ft_free(char ****new_str, int k)
 {
-	int	i;
-
-	i = 0;
-	while (new[i])
-		free(new[i++]);
+	while (--k < -1)
+		free(**new_str[k]);
+	free(*new_str[k]);
 }
 
-static int	ft_str_counter(char **new_str, char const *s, char c, int k)
+static int	ft_str_counter(char const *s, char c)
 {
 	int	i;
-	int	j;
 	int	counter_str;
 
 	i = 0;
 	counter_str = 0;
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		while (s[i] == c)
 			i++;
 		if (s[i])
-			j = i;
-		if (s[i])
 			counter_str++;
 		while (s[i] && s[i] != c)
 			i++;
-		if (new_str && counter_str)
-		{
-			new_str[k] = ft_substr(s, j, i - j);
-			if (!new_str[k++])
-			{
-				ft_free(new_str);
-				break ;
-			}
-		}
 	}
 	return (counter_str);
+}
+
+static char	**ft_cpy_strofstr(char ***new_str, char const *s, char c, int k)
+{
+	int	i;
+
+	i = 0;
+	printf("Inicio\n");
+	while (*s)
+	{
+		printf("s = %p\n", s);
+		while (*s == c)
+			s += 1;
+		printf("s = %p\n", s);
+		while (s[i] && s[i] != c)
+			i++;
+		printf("word len = %i\n", i);
+		if (new_str && *s)
+		{
+			*new_str[k] = ft_substr(s, 0, i);
+			if (!*new_str[k])
+			{
+				ft_free(&new_str, k);
+				break ;
+			}
+			printf("new_str[%i] = %s\n", k, *new_str[k]);
+			k++;
+			s += i;
+		}
+	}
+	return (*new_str);
 }
 
 char	**ft_split(const char *s, char c)
@@ -58,25 +75,31 @@ char	**ft_split(const char *s, char c)
 	char	**new_str;
 	size_t	i;
 
+	printf("Inicio\n");
 	if (!s)
 		return (0);
 	new_str = NULL;
-	i = ft_str_counter(new_str, s, c, 0);
+	i = ft_str_counter(s, c);
+	printf("i = %lu\n", i);
 	new_str = malloc(sizeof(char *) * (i + 1));
 	if (!new_str)
-		return (0);
-	ft_str_counter(new_str, s, c, 0);
+		return (NULL);
+	printf("new_str address\t%p\n", new_str);
 	if (new_str)
-	new_str[i] = 0;
+	{
+		new_str = ft_cpy_strofstr(&new_str, s, c, 0);
+		if (new_str)
+			new_str[i] = 0;
+	}
 	return (new_str);
 }
 
-/* int	main(void)
+int	main(void)
 {
 	int		i;
 	char	**str;
 
-	str = ft_split("tripouille", 0);
+	str = ft_split("  tripouille  42  ", ' ');
 	i = 0;
 	while (str[i])
 	{
@@ -88,4 +111,4 @@ char	**ft_split(const char *s, char c)
 		while (--i < -1)
 			free(str[i]);
 	free(str);
-} */
+}
