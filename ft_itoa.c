1064a3aeaab8cee44137aa1bc238cc92cf3af681
Miguel Silva <miguel1998282@gmail.com>
2022-09-01 22:34:09 +0100

    Update Itoa
    Done itoa with recursion

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,44 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtavares <mtavares@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mtavares <mtavares@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 12:07:29 by mtavares          #+#    #+#             */
-/*   Updated: 2022/03/06 19:39:33 by mtavares         ###   ########.fr       */
+/*   Updated: 2022/09/01 22:32:44 by mtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_numdig(int n)
+static char	*ft_itoa_aux(char *str, int *numdig, int counter, int n)
 {
-	if (n < 10 && n > -10)
-		return (1);
-	else
-		return (1 + ft_numdig(n / 10));
+	*numdig = counter;
+	if (n > 9)
+		str = ft_itoa_aux(str, numdig, counter + 1, n / 10);
+	if (!str)
+		str = ft_calloc(sizeof(char), counter + 1);
+	if (!str)
+		return (NULL);
+	str[*numdig - counter] = (n % 10) + 48;
+	return (str);
 }
 
 char	*ft_itoa(int n)
 {
-	char	*num;
-	int		num_dig;
+	char	*str;
 	int		i;
-	int		signal;
 
-	signal = n;
-	num_dig = ft_numdig(n) + 1 + (n < 0);
-	num = (char *)malloc(sizeof(char) * num_dig);
-	if (!num)
-		return (NULL);
-	num[num_dig - 1] = '\0';
-	i = num_dig - 2;
-	while (i > -1)
-	{
-		num[i] = ((n % 10) * ((n > 0) - (n < 0))) + 48;
-		n /= 10;
-		i--;
-	}
-	if (signal < 0)
-		num[0] = '-';
-	return (num);
+	i = 0;
+	str = ft_itoa_aux(NULL, &i, 1, n);
+	return (str);
 }
