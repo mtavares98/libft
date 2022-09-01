4e1fd373426767ee2f70e552fb5afffe4a0fde81
Miguel Silva <miguel1998282@gmail.com>
2022-09-02 00:42:48 +0100

    Update
    Missing testing, but it will be according with the manual of calloc in linux

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtavares <mtavares@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mtavares <mtavares@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 14:16:53 by mtavares          #+#    #+#             */
-/*   Updated: 2022/06/07 22:13:51 by mtavares         ###   ########.fr       */
+/*   Updated: 2022/09/01 23:32:18 by mtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	size_t	total;
+	int		total;
 	void	*ptr;
 
-	total = count * size;
-	if (size != 0 && total / size != count)
+	total = (int)(count * size);
+	if (size != 0 && count != 0 && total / size != count)
 		return (NULL);
 	ptr = malloc(total);
 	if (ptr)
