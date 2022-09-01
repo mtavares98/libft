491104561aabda20f9f8179bf0ee70fff3332d7c
Miguel Silva <miguel1998282@gmail.com>
2022-09-02 00:45:38 +0100

    Update ft_bzero
    Minor change

--- ft_bzero.c
+++ ft_bzero.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtavares <mtavares@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mtavares <mtavares@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 18:53:14 by mtavares          #+#    #+#             */
-/*   Updated: 2022/04/16 14:21:02 by mtavares         ###   ########.fr       */
+/*   Updated: 2022/09/01 23:28:12 by mtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t			i;
-	unsigned char	*str;
+	size_t	i;
+	char	*str;
 
 	str = s;
 	i = -1;
